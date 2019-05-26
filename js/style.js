11dfa514b153817fc12e98ca38ea1f805d4b3932
Luan F. R. Domingues <Lrochadomingues@gmail.com>
2019-05-25 20:45:21 -0400

    Fixed layout bug when orientation changed

--- js/style.js
+++ js/style.js
@@ -1,12 +1,29 @@
 (function(){
 
-	let height = document.documentElement.clientHeight;
-	let body = document.getElementById('body');
+	let height = window.innerHeight;
+	let body = document.getElementById("body");
 
-	if(height > 460){
-
-		body.style.gridTemplateRows = height + "px "+ "auto";
-		
+	if (height > 460) {
+		body.style.gridTemplateRows = height + "px "+ "auto";	
 	}
-	
+
+	window.addEventListener("orientationchange", function() {
+		var afterOrientationChange = function(){
+			let heightAfter = window.innerHeight;
+
+			if (heightAfter < 460) {
+				body.style.gridTemplateRows =  "";
+			}
+			else if(heightAfter > 460) {
+				body.style.gridTemplateRows = heightAfter + "px "+ "auto";
+			}
+			window.removeEventListener("resize", afterOrientationChange);
+			console.log(heightAfter);
+		};
+
+		window.addEventListener("resize", afterOrientationChange);
+
+	});
+
+	document.getElementById("style").href='./CSS/style.css';
 })();
